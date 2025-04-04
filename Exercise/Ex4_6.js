7a17f8989659c24d6abb5be1c704c879fe921a46
binhlethanh201 <ltbinh04@gmail.com>
2025-04-04 21:33:05 +0700

    Create Ex4_6.js

---
+++ Exercise/Ex4_6.js
@@ -0,0 +1,13 @@
+var array = [1, 2, 4, 6, 8, 12, 22, 67, 60, 99, 30, 2, 45, 56, 12, 60, 31, 12];
+
+//In ra tất cả phần tử trong mảng.
+console.log(array);
+
+//Tạo một mảng mới chứa tất cả phần tử có giá trị lớn hơn 10.
+let array1 = [];
+for(let i =0; i<array.length; i++){
+    if(array[i] > 10 ){
+        array1.push(array[i]);
+    }
+}
+console.log(array1);
